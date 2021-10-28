c4e56533909f6fd86441e55359184618a304a2cd
Leonid Ivanov <l.ivanov@metrotek.ru>
2021-10-28 14:33:49 +0300

    moved multiline_buf into px_to_dct_adapter

--- src/dct_2d.sv
+++ src/dct_2d.sv
@@ -25,28 +25,6 @@ localparam int DCT_1D_T_TDATA_WIDTH = DCT_1D_WIDTH * 8;
                                 
 
 axi4_stream_if #(
-  .TDATA_WIDTH ( PX_TDATA_WIDTH ),
-  .TID_WIDTH   ( 1              ),
-  .TDEST_WIDTH ( 1              ),
-  .TUSER_WIDTH ( 1              )
-) multi_line_video[7 : 0] (
-  .aclk        ( clk_i          ),
-  .aresetn     ( !rst_i         )
-);
-
-multiline_buf #(
-  .BUF_AMOUNT    ( 8                ),
-  .LINES_PER_BUF ( 2                ),
-  .PX_WIDTH      ( PX_WIDTH         ),
-  .FRAME_RES_X   ( FRAME_RES_X      )
-) multiline_buf_inst (
-  .clk_i         ( clk_i            ),
-  .rst_i         ( rst_i            ),
-  .video_i       ( video_i          ),
-  .video_o       ( multi_line_video )
-);
-
-axi4_stream_if #(
   .TDATA_WIDTH ( PAR_PX_TDATA_WIDTH ),
   .TID_WIDTH   ( 1                  ),
   .TDEST_WIDTH ( 1                  ),
@@ -57,11 +35,12 @@ axi4_stream_if #(
 );
 
 px_to_dct_adapter #(
-  .PX_WIDTH    ( PX_WIDTH               )
+  .PX_WIDTH    ( PX_WIDTH               ),
+  .FRAME_RES_X ( FRAME_RES_X            )
 ) line_selector (
   .clk_i       ( clk_i                  ),
   .rst_i       ( rst_i                  ),
-  .ser_video_i ( multi_line_video       ),
+  .ser_video_i ( video_i                ),
   .par_video_o ( parallel_shifted_video )
 );
 
--- src/px_to_dct_adapter.sv
+++ src/px_to_dct_adapter.sv
@@ -5,40 +5,63 @@
 */
 
 module px_to_dct_adapter #(
-  parameter int PX_WIDTH = 8
+  parameter int PX_WIDTH    = 8,
+  parameter int FRAME_RES_X = 1280
 )(
   input clk_i,
   input rst_i,
-  axi4_stream_if.slave  ser_video_i [7 : 0],
+  axi4_stream_if.slave  ser_video_i,
   axi4_stream_if.master par_video_o
 );
 
-localparam int SER_TDATA_WIDTH   = PX_WIDTH % 8 ?
-                                   ( PX_WIDTH / 8 + 1 ) * 8 :
-                                   PX_WIDTH;
-localparam int SER_TDATA_WIDTH_B = SER_TDATA_WIDTH / 8;
-localparam int PAR_TDATA_WIDTH   = PX_WIDTH * 8;
-localparam int VALUE_SHIFT       = 2 ** PX_WIDTH / 2;
+localparam int PX_TDATA_WIDTH   = PX_WIDTH % 8 ?
+                                  ( PX_WIDTH / 8 + 1 ) * 8 :
+                                  PX_WIDTH;
+localparam int PX_TDATA_WIDTH_B = PX_TDATA_WIDTH / 8;
+localparam int PAR_TDATA_WIDTH  = PX_WIDTH * 8;
+localparam int VALUE_SHIFT      = 2 ** PX_WIDTH / 2;
 
-logic [2 : 0]                          px_cnt;
-logic [2 : 0]                          ln_cnt;
-logic [7 : 0][PX_WIDTH - 1 : 0]        packed_par_tdata;
-logic [7 : 0][SER_TDATA_WIDTH - 1 : 0] ser_video_tdata;
-logic [7 : 0]                          ser_video_tvalid;
-logic [7 : 0]                          ser_video_tready;
-logic [7 : 0]                          ser_video_tlast;
-logic [7 : 0]                          ser_video_tuser;
+logic [2 : 0]                         px_cnt;
+logic [2 : 0]                         ln_cnt;
+logic [7 : 0][PX_WIDTH - 1 : 0]       packed_par_tdata;
+logic [7 : 0][PX_TDATA_WIDTH - 1 : 0] multi_line_video_tdata;
+logic [7 : 0]                         multi_line_video_tvalid;
+logic [7 : 0]                         multi_line_video_tready;
+logic [7 : 0]                         multi_line_video_tlast;
+logic [7 : 0]                         multi_line_video_tuser;
+
+axi4_stream_if #(
+  .TDATA_WIDTH ( PX_TDATA_WIDTH ),
+  .TID_WIDTH   ( 1              ),
+  .TDEST_WIDTH ( 1              ),
+  .TUSER_WIDTH ( 1              )
+) multi_line_video [7 : 0] (
+  .aclk        ( clk_i          ),
+  .aresetn     ( !rst_i         )
+);
+
+multiline_buf #(
+  .BUF_AMOUNT    ( 8                ),
+  .LINES_PER_BUF ( 2                ),
+  .PX_WIDTH      ( PX_WIDTH         ),
+  .FRAME_RES_X   ( FRAME_RES_X      )
+) multiline_buf_inst (
+  .clk_i         ( clk_i            ),
+  .rst_i         ( rst_i            ),
+  .video_i       ( ser_video_i      ),
+  .video_o       ( multi_line_video )
+);
 
 genvar g;
 
 generate
   for( g = 0; g < 8; g++ )
     begin : if_unpack
-      assign ser_video_tdata[g]    = ser_video_i[g].tdata;
-      assign ser_video_tvalid[g]   = ser_video_i[g].tvalid;
-      assign ser_video_i[g].tready = ser_video_tready[g];
-      assign ser_video_tlast[g]    = ser_video_i[g].tlast;
-      assign ser_video_tuser[g]    = ser_video_i[g].tuser;
+      assign multi_line_video_tdata[g]  = multi_line_video[g].tdata;
+      assign multi_line_video_tvalid[g] = multi_line_video[g].tvalid;
+      assign multi_line_video[g].tready = multi_line_video_tready[g];
+      assign multi_line_video_tlast[g]  = multi_line_video[g].tlast;
+      assign multi_line_video_tuser[g]  = multi_line_video[g].tuser;
     end
 endgenerate
 
@@ -46,22 +69,22 @@ always_ff @( posedge clk_i, posedge rst_i )
   if( rst_i )
     px_cnt <= 3'd0;
   else
-    if( ser_video_tvalid[ln_cnt] && ser_video_tready[ln_cnt] )
+    if( multi_line_video_tvalid[ln_cnt] && multi_line_video_tready[ln_cnt] )
       px_cnt <= px_cnt + 1'b1;
 
 always_ff @( posedge clk_i, posedge rst_i )
   if( rst_i )
     ln_cnt <= 3'd0;
   else
-    if( ser_video_tvalid[ln_cnt] && ser_video_tready[ln_cnt] && px_cnt == 3'd7 )
+    if( multi_line_video_tvalid[ln_cnt] && multi_line_video_tready[ln_cnt] && px_cnt == 3'd7 )
       ln_cnt <= ln_cnt + 1'b1;
 
 always_ff @( posedge clk_i, posedge rst_i )
   if( rst_i )
     packed_par_tdata <= PAR_TDATA_WIDTH'( 0 );
   else
-    if( ser_video_tready[ln_cnt] )
-      packed_par_tdata[px_cnt] <= ser_video_tdata[ln_cnt][PX_WIDTH - 1 : 0] - PX_WIDTH'( VALUE_SHIFT );
+    if( multi_line_video_tready[ln_cnt] )
+      packed_par_tdata[px_cnt] <= multi_line_video_tdata[ln_cnt][PX_WIDTH - 1 : 0] - PX_WIDTH'( VALUE_SHIFT );
 
 assign par_video_o.tdata = packed_par_tdata;
 
@@ -69,7 +92,7 @@ always_ff @( posedge clk_i, posedge rst_i )
   if( rst_i )
     par_video_o.tvalid <= 1'b0;
   else
-    if( px_cnt == 3'd7 && ser_video_tvalid[ln_cnt] && ser_video_tready[ln_cnt] )
+    if( px_cnt == 3'd7 && multi_line_video_tvalid[ln_cnt] && multi_line_video_tready[ln_cnt] )
       par_video_o.tvalid <= 1'b1;
     else
       if( par_video_o.tready )
@@ -79,7 +102,7 @@ always_ff @( posedge clk_i, posedge rst_i )
   if( rst_i )
     par_video_o.tlast <= 1'b0;
   else
-    if( ser_video_tvalid[7] && ser_video_tready[7] && ser_video_tlast[7] )
+    if( multi_line_video_tvalid[7] && multi_line_video_tready[7] && multi_line_video_tlast[7] )
       par_video_o.tlast <= 1'b1;
     else
       if( par_video_o.tvalid && par_video_o.tready )
@@ -89,7 +112,8 @@ always_ff @( posedge clk_i, posedge rst_i )
   if( rst_i )
     par_video_o.tuser <= 1'b0;
   else
-    if( ser_video_tvalid[ln_cnt] && ser_video_tready[ln_cnt] && ser_video_tuser[ln_cnt] )
+    if( multi_line_video_tvalid[ln_cnt] && multi_line_video_tready[ln_cnt] && 
+        multi_line_video_tuser[ln_cnt] )
       par_video_o.tuser <= 1'b1;
     else
       if( par_video_o.tvalid && par_video_o.tready )
@@ -98,8 +122,8 @@ always_ff @( posedge clk_i, posedge rst_i )
 always_comb
   for( int i = 0 ; i < 8; i++ )
     if( 3'( i ) == ln_cnt )
-      ser_video_tready[i] = par_video_o.tready || !par_video_o.tvalid;
+      multi_line_video_tready[i] = par_video_o.tready || !par_video_o.tvalid;
     else
-      ser_video_tready[i] = 1'b0;
+      multi_line_video_tready[i] = 1'b0;
 
 endmodule
